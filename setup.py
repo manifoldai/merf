3cf13a8585cdf644c03442fb0a61bfcf3203234d
Sourav Dey <souravdey@gmail.com>
2017-10-16 10:31:42 -0700

    Adding setup.py and MANIFEST file for packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='merf',
+      version='0.1',
+      description='Mixed Effects Random Forest',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'],
+      keywords='random forest machine learning mixed effects',
+      url='https://github.com/manifoldai/merf',
+      author='Manifold, Inc.',
+      author_email='sdey@manifold.ai',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['pandas', 'numpy', 'sklearn'],
+      include_package_data=True,
+      zip_safe=False)
