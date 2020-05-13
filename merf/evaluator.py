6d56152fd6ac26be8739d497e1375f7cd6b24656
Sourav Dey <sdey@manifold.ai>
2020-05-12 22:14:48 -0700

    Remove panel code and replace with multiindexed dataframe in evaluator

--- merf/evaluator.py
+++ merf/evaluator.py
@@ -51,12 +51,23 @@ def plot_bhat(model, cluster_id):
     :param cluster_id:
     :return:
     """
-    b_df = np.dstack(model.b_hat_history)
-    b_panel = pd.Panel(b_df, items=model.b_hat_history[0].index)
-    plt.plot(b_panel.iloc[cluster_id].T)
+    # This code does a complicated reshape and re-indexing operation to get the
+    # list of dataframes into a multi-indexed dataframe.
+    # Step 1 - vertical stack all the arrays at each iteration into a single numpy array
+    b_array = np.vstack(model.b_hat_history)
+
+    # Step 2 - Create the multi-index. Note the outer index is iteration. The inner index is cluster.
+    iterations = range(len(model.b_hat_history))
+    clusters = model.b_hat_history[0].index
+    mi = pd.MultiIndex.from_product([iterations, clusters], names=("iteration", "cluster"))
+
+    # Step 3 - Create the multi-indexed dataframe
+    b_df = pd.DataFrame(b_array, index=mi)
+
+    # Step 4 - Use the fancy xs function to access all iterations of a single cluster
+    b_df.xs(cluster_id, level="cluster").plot()
     plt.grid("on")
     plt.ylabel("b_hat")
     plt.xlabel("Iteration")
-    plt.legend()
-    plt.title("Cluster = {}".format(model.cluster_counts.index[cluster_id]))
-    return b_panel
+    plt.title("Cluster = {}".format(cluster_id))
+    return b_df
